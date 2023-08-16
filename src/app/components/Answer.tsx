c05ba778baf34c1f69bc6e995bea4cedfaf76ed0
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2023-08-16 17:05:26 +0500

    fix/border colors

--- src/app/components/Answer.tsx
+++ src/app/components/Answer.tsx
@@ -23,7 +23,7 @@ const Answer: React.FC<AnswerProps> = ({
         onStepChange();
         handleCorrectAnswer();
       }}
-      className="py-1.5 px-3 text-xl sm:text-2xl border-[0.5rem] border-indigo-800 rounded-[1.25rem] cursor-pointer hover:translate-x-3 hover:bg-blue-900 hover:text-white hover:border-indigo-700 transition-all duration-300"
+      className="py-1.5 px-3 text-xl sm:text-2xl border-[0.5rem] border-indigo-600 rounded-[1.25rem] cursor-pointer hover:translate-x-3 hover:bg-blue-900 hover:text-white hover:border-indigo-500 transition-all duration-300"
     >
       {item.answer}
     </li>
